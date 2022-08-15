6105328314471682edddef4b65075a40e66b9e72
Roman <sporco1988@gmail.com>
2022-08-15 23:13:37 +0700

    added ArrayListWrapper

--- 04-DynArrays/src/Program.java
+++ 04-DynArrays/src/Program.java
@@ -4,18 +4,47 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        IArray singleArray = new SingleArray();
-        IArray vectorArray = new VectorArray();
-        IArray factorArray = new FactorArray();
-        IArray matrixArray = new MatrixArray();
+        //testAdd();
 
-        testAddIndex0Array(matrixArray, 20);
+        testAddIndex0Array();
+        //IArray singleArray = new SingleArray();
+        //IArray vectorArray = new VectorArray();
+        //IArray factorArray = new FactorArray();
+        //IArray matrixArray = new MatrixArray();
+
+        //testAddIndex0Array(matrixArray, 20);
 
         //testAddArray(singleArray, 10_000);
         //testAddArray(vectorArray, 100_000);
         //testAddArray(factorArray, 100_000);
         //testAddArray(matrixArray, 100_000);
+
+
     }
+
+    private static void testAdd() {
+        for(int i = 1_000; i <= 10_000_000; i *= 10) {
+            if(i <= 100_000) {
+                IArray singleArray = new SingleArray();
+                testAddArray(singleArray, i);
+            }
+
+            if(i <= 1_000_000) {
+                IArray vectorArray = new VectorArray();
+                testAddArray(vectorArray, i);
+            }
+
+            IArray factorArray = new FactorArray();
+            testAddArray(factorArray, i);
+
+            IArray matrixArray = new MatrixArray();
+            testAddArray(matrixArray, i);
+
+            IArray arrayListWrapper = new ArrayListWrapper();
+            testAddArray(arrayListWrapper, i);
+        }
+    }
+
 
     private static void testAddArray(IArray data, int total) {
         long start = System.currentTimeMillis();
@@ -23,8 +52,34 @@ public class Program {
         for (int j = 0; j < total; j ++)
             data.add(new Date());
 
-        System.out.println(data + " testAddArray: " +
+        System.out.println(data + " Elements:" + total  + " testAddArray: " +
                 (System.currentTimeMillis() - start));
+    }
+
+    private static void testAddIndex0Array() {
+        for(int i = 1_000; i <= 1_000_000; i *= 10) {
+            if(i <= 10_000) {
+                IArray singleArray = new SingleArray();
+                testAddIndex0Array(singleArray, i);
+            }
+
+            if(i <= 100_000) {
+                IArray vectorArray = new VectorArray();
+                testAddIndex0Array(vectorArray, i);
+            }
+
+            IArray factorArray = new FactorArray();
+            testAddIndex0Array(factorArray, i);
+
+            if(i <= 100_000) {
+                IArray matrixArray = new MatrixArray();
+                testAddIndex0Array(matrixArray, i);
+            }
+
+
+            IArray arrayListWrapper = new ArrayListWrapper();
+            testAddIndex0Array(arrayListWrapper, i);
+        }
     }
 
     private static void testAddIndex0Array(IArray data, int total) {
@@ -32,9 +87,6 @@ public class Program {
 
         for (int j = 0; j < total; j ++)
             data.add(j, 0);
-
-        var t = data.remove(2);
-        data.add(t, 2);
 
         System.out.println(data + " Elements:" + total  + " testAddIndex0Array: " +
                 (System.currentTimeMillis() - start));
